709b8deaba5ebf573d7716f926ba30907ecf7e85
Lyrics Duet <68720020+rajpatel72@users.noreply.github.com>
2025-10-24 20:59:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('signup').addEventListener('click', () => {
+  alert('Signup Docs button clicked!');
+});
+
+document.getElementById('quote').addEventListener('click', () => {
+  alert('Quote button clicked!');
+});
+
+document.getElementById('roll').addEventListener('click', () => {
+  alert('Roll-in/Roll-out button clicked!');
+});
