fee4aceba581dfc5019aa57412f60ead2881010e
Lyrics Duet <68720020+rajpatel72@users.noreply.github.com>
2025-10-31 23:27:46 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
